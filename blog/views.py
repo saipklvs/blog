e89d6c156dc04f004023ef5f39edd00f243f6fe6
saipklvs <pavanlottala94@gmail.com>
2020-04-05 00:50:12 -0500

    main page

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,26 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author':'Sai',
+        'title':'Blog Post'
+    }, 
+    {
+        'author':'Sai',
+        'title':'Blog Post 2'
+    }
+]
+
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> BLog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
