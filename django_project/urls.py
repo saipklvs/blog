8beb75b2e2a0910b8cf701ce1610022456a9b4b2
saipklvs <pavanlottala94@gmail.com>
2020-05-04 20:33:18 -0500

    completed the login and log out system

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from users import views as user_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^register/', user_views.register, name='register'),
+    url(r'^profile/', user_views.profile, name='profile'),
+    url(r'^login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    url(r'^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     url(r'^', include('blog.urls')),
 ]
