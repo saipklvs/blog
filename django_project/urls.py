1438cd938990c0720d5bb1f12efe696ba6453cac
saipklvs <pavanlottala94@gmail.com>
2020-05-05 18:16:45 -0500

    User profile done

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from users import views as user_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -27,3 +28,7 @@ urlpatterns = [
     url(r'^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     url(r'^', include('blog.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
