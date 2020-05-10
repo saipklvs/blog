1e57a993fa74e07d4f9862899416d3b3871677e6
saipklvs <pavanlottala94@gmail.com>
2020-05-09 20:23:51 -0500

    some problems with page psoting

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User 
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -14,3 +15,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,18 @@
 from django.conf.urls import url
 from . import views
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView, 
+    PostUpdateView,
+    PostDeleteView
+    )
 
 urlpatterns = [
-    url(r'^$', views.home, name='blog-home'),
+    url(r'^$', PostListView.as_view(), name='blog-home'),
+    url(r'^post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    url(r'^post/new/', PostCreateView.as_view(), name='post-create'),
+    url(r'^post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    url(r'^post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     url(r'^about/', views.about, name='blog-about'),
 ] 
